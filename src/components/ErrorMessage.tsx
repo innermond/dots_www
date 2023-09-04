00f3f1541951c24744748eddce2cb75e1e52baa5
gabriel <innermond@yahoo.com>
2023-09-04 20:22:57 +0300

    prettierrc

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,16 +1,16 @@
-import type { Component } from 'solid-js';
-import { For } from 'solid-js';
+import type { Component } from "solid-js";
+import { For } from "solid-js";
 
 const ErrorMessage: Component<ErrorProps> = (props) => {
   return (
-  <For each={props.errors}>
-    {(error) => <div class="error-message">{error.message}</div>}
+    <For each={props.errors}>
+      {(error) => <div class="error-message">{error.message}</div>}
     </For>
-  )
+  );
 };
 
 type ErrorProps = {
-  errors: Error[]
+  errors: Error[];
 };
 
 export default ErrorMessage;
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,27 +1,39 @@
-import './LoginForm.scss';
-import type { Component } from 'solid-js';
+import "./LoginForm.scss";
+import type { Component } from "solid-js";
 
 const LoginForm: Component = () => {
   return (
-     <form action="#" method="post">
+    <form action="#" method="post">
+      <div class="container">
+        <label for="uname">Username</label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
 
-  <div class="container">
-    <label for="uname">Username</label>
-    <input type="text" placeholder="Enter Username" name="uname" required />
+        <label for="psw">Password</label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
 
-    <label for="psw">Password</label>
-    <input type="password" placeholder="Enter Password" name="psw" required />
+        <button type="submit">Login</button>
+        <label>
+          <input type="checkbox" name="remember">
+            Remember me
+          </input>
+        </label>
+      </div>
 
-    <button type="submit">Login</button>
-    <label><input type="checkbox" name="remember">Remember me</input></label>
-  </div>
-
-  <div class="container">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-  )
+      <div class="container">
+        <button type="button" class="cancelbtn">
+          Cancel
+        </button>
+        <span class="psw">
+          Forgot <a href="#">password?</a>
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
