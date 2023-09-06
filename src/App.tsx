eb0e78f66797b4a29b1c219a04c5e86a7c865bed
gabriel <innermond@yahoo.com>
2023-09-06 22:30:58 +0300

    add suid login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import type { Component } from 'solid-js';
+import { CssBaseline } from '@suid/material';
 
 import LoginForm from './components/LoginForm';
 
 const App: Component = () => {
-  return <LoginForm />;
+  return (
+  <>
+  <CssBaseline />
+  <LoginForm />
+  </>);
 };
 
 export default App;
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,22 +1,72 @@
 import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
-import { Button, Backdrop } from '@suid/material';
+import {} from 'solid-js';
+import { Typography, Link, Container, Box, Avatar, TextField, FormControlLabel, Checkbox, Button, Grid } from '@suid/material';
+import { createTheme, ThemeProvider } from '@suid/material/styles';
+import LockOutlinedIcon from '@suid/icons-material/LockOutlined';
+
+const Copyright: Component = (props: any) => {
+  return (
+    <Typography variant="body2" color="text.secundary" align="center" {...props}>
+      {'Copyright © '}
+      <Link href="https://volt-media.ro">volt-media.ro</Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+};
+
+const defaultTheme = createTheme();
 
 const LoginForm: Component = () => {
-  const [open, setOpen] = createSignal(false);
-  const openBackdrop = () => setOpen(true);
-  const closeBackdrop = () => setOpen(false);
-
   return (
-  <>
-    <Button 
-      onClick={openBackdrop}
+  <ThemeProvider theme={defaultTheme}>
+  <Container component="main" maxWidth="xs">
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
     >
-    {open() ? 'opened' : 'closed'}
-    </Button>
-
-    <Backdrop open={open()} onClick={closeBackdrop}></Backdrop>
-    </>
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">Sign In</Typography>
+      <Box component="form" sx={{ mt: 1 }} >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>Sign In</Button>
+        <Grid container>
+          <Grid item xs><Link variant="body2" href="#">Forgot password?</Link></Grid>
+          <Grid item xs><Link variant="body2" href="#">Don't have an account? Sign up</Link></Grid>
+        </Grid>
+      </Box>
+    </Box>
+    <Copyright sx={{mt: 8, mb:4}}/>
+  </Container>
+  </ThemeProvider>
   );
 };
 
