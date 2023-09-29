ccdcb74f2f7bcbd1a576cafda657bf68bea9629f
gabriel <innermond@yahoo.com>
2023-09-29 19:48:09 +0300

    prettify

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -4,19 +4,9 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import type { Accessor, Component } from 'solid-js';
-import {
-  Match,
-  Switch,
-  ErrorBoundary,
-  createSignal,
-} from 'solid-js';
+import { Match, Switch, ErrorBoundary, createSignal } from 'solid-js';
 import { Routes, Route, useRouteData, Navigate } from '@solidjs/router';
-import {
-  Alert,
-  Box,
-  CircularProgress,
-  CssBaseline,
-} from '@suid/material';
+import { Alert, Box, CircularProgress, CssBaseline } from '@suid/material';
 import { ThemeProvider } from '@suid/material/styles';
 import { Toaster } from 'solid-toast';
 
@@ -27,7 +17,7 @@ import Loading from './components/Loading';
 import { defaultTheme } from './theme';
 
 function TokenData() {
-  const [token, setToken] = createSignal("");
+  const [token, setToken] = createSignal('');
   const key = 'dots.tok';
   const tok = sessionStorage.getItem(key);
   setToken(tok ?? '');
@@ -41,7 +31,10 @@ const App: Component = () => {
       <Routes>
         <Route path="/" element={Guard(Dashboard)} data={TokenData} />
         <Route path="/login" component={LoginForm} />
-        <Route path="/*" component={() => <Alert severity="warning">Not found</Alert>} />
+        <Route
+          path="/*"
+          component={() => <Alert severity="warning">Not found</Alert>}
+        />
       </Routes>
       <Toaster />
       <Loading />
