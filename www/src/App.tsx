b8498ba0cbd323c8416109779c5dc4f30a1c0392
innermond <innermond@yahoo.com>
2023-09-30 15:06:39 +0300

    add: router use routes definition

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -3,9 +3,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import type { Accessor, Component } from 'solid-js';
+import type { Accessor, Component, JSX } from 'solid-js';
 import { Match, Switch, ErrorBoundary, createSignal } from 'solid-js';
-import { Routes, Route, useRouteData, Navigate } from '@solidjs/router';
+import { useRoutes, Router, useRouteData, Navigate, RouteDefinition } from '@solidjs/router';
 import { Alert, Box, CircularProgress, CssBaseline } from '@suid/material';
 import { ThemeProvider } from '@suid/material/styles';
 import { Toaster } from 'solid-toast';
@@ -24,44 +24,26 @@ function TokenData() {
   return token;
 }
 
-const App: Component = () => {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <Routes>
-        <Route path="/" element={Guard(Dashboard)} data={TokenData} />
-        <Route path="/login" component={LoginForm} />
-        <Route
-          path="/*"
-          component={() => <Alert severity="warning">Not found</Alert>}
-        />
-      </Routes>
-      <Toaster />
-      <Loading />
-    </ThemeProvider>
-  );
-};
+function Guard(child: Component): Component {
+  return  (): JSX.Element => {
+    const token: any = useRouteData();
 
-export default App;
-
-function Guard(child: Component) {
-  const token: any = useRouteData();
-
-  return (
-    <ErrorBoundary
-      fallback={err => <Alert severity="error">{err.message}</Alert>}
-    >
-      <Switch fallback={<Progress />}>
-        <Match when={!!token()}>{child}</Match>
-        <Match when={token() === ''}>
-          <Navigate href="/login" />
-        </Match>
-      </Switch>
-    </ErrorBoundary>
-  );
+    return (
+      <ErrorBoundary
+        fallback={err => <Alert severity="error">{err.message}</Alert>}
+      >
+        <Switch fallback={<Progress />}>
+          <Match when={!!token()}>{child}</Match>
+          <Match when={token() === ''}>
+            <Navigate href="/login" />
+          </Match>
+        </Switch>
+      </ErrorBoundary>
+    );
+  };
 }
 
-function Progress() {
+function Progress(): JSX.Element {
   return (
     <Box
       sx={{
@@ -75,3 +57,26 @@ function Progress() {
     </Box>
   );
 }
+
+const routes: RouteDefinition[] = [
+  {path: "/", component: Guard(Dashboard), data: TokenData},
+  {path: "/login", component: LoginForm},
+  {path: "/*", component: () => <Alert severity="warning">Not found</Alert>},
+];
+
+const App: Component = (): JSX.Element => {
+  const Routes = useRoutes(routes);
+
+  return (
+    <Router>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Routes />
+        <Toaster />
+        <Loading />
+      </ThemeProvider>
+    </Router>
+  );
+};
+
+export default App;
--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,6 +1,5 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-import { Router } from '@solidjs/router';
 
 import './index.css';
 import App from './App';
@@ -14,10 +13,6 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 }
 
 render(
-  () => (
-    <Router>
-      <App />
-    </Router>
-  ),
+  () => <App />,
   root!,
 );
