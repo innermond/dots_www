7b3e9e9e80eb5965bc593627de0418fd21d2b645
gabriel <innermond@yahoo.com>
2023-10-06 19:24:24 +0300

    added currentPage to state

--- www/src/lib/app.ts
+++ www/src/lib/app.ts
@@ -1,7 +1,23 @@
 import { createSignal, createRoot } from 'solid-js';
+import type { Accessor, Setter } from 'solid-js';
+import type { CompanyData } from '../pages/company/types'; 
+import { companyZero } from '../pages/company/types'; 
 
 const [ currentPageTitle, setCurrentPageTitle ] = createSignal<string>("");
+const [ currentCompany, setCurrentCompany ] = createSignal<CompanyData>(companyZero);
 
-const state =  createRoot(() => ({currentPageTitle, setCurrentPageTitle}));
+type AppState = {
+  currentPageTitle: Accessor<string>,
+  setCurrentPageTitle: Setter<string>,
+  currentCompany: Accessor<CompanyData>,
+  setCurrentCompany: Setter<CompanyData>,
+};
+
+const state =  createRoot((): AppState => ({
+  currentPageTitle,
+  setCurrentPageTitle,
+  currentCompany,
+  setCurrentCompany,
+}));
 
 export default state;
