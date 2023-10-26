9d5d8a60e44e4d15815fa8cf8ae8785c1723b7d2
gabriel <innermond@yahoo.com>
2023-10-26 21:23:09 +0300

    theme customisation

--- www/src/theme.ts
+++ www/src/theme.ts
@@ -1,8 +1,15 @@
 import { createTheme } from '@suid/material/styles';
+import Typography from './typography';
+import Palette from './palette';
 
-export const defaultTheme = createTheme();
+export const defaultTheme = createTheme({
+  typography: Typography(`'Public Sans', sans-serif`),
+  palette: Palette('light', 'default'),
+});
 /*const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });*/
+
+
