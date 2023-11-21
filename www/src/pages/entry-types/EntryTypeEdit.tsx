9214b831f8e433ff494dc7c0d84f21c267114842
gabriel <innermond@yahoo.com>
2023-11-21 22:23:15 +0200

    let the changes be handled by outer component by droping handleInput and its bindings + update state with data from server after edit

--- www/src/pages/entry-types/EntryTypeEdit.tsx
+++ www/src/pages/entry-types/EntryTypeEdit.tsx
@@ -3,22 +3,15 @@ import { createEffect } from 'solid-js';
 import type { JSX } from 'solid-js';
 import InputOrSelect from './InputOrSelect';
 import type { EntryTypeData } from '@/pages/entry-types/types';
-import {
-  isEntryTypeData,
-  isKeyofEntryTypeData,
-} from '@/pages/entry-types/types';
+import { isEntryTypeData } from '@/pages/entry-types/types';
 
 import TextFieldEllipsis from '@/components/TextFieldEllipsis';
 import { produce } from 'solid-js/store';
-import type {
-  FieldNames,
-  MessagesMap,
-  Validators,
-  Validation,
-} from '@/lib/form';
+import type { FieldNames, MessagesMap, Validators } from '@/lib/form';
 import { required, minlen, maxlen, optional, int } from '@/lib/form';
 import toasting from '@/lib/toast';
 import { DialogProviderValue, useDialog } from '@/contexts/DialogContext';
+import { makeDefaults } from '@/lib/form/form';
 
 const theme = useTheme();
 const names = ['id', 'code', 'description', 'unit'];
@@ -55,19 +48,6 @@ export default function EntryTypeEdit(): JSX.Element {
 
   setValidation({ validators, messages });
 
-  const handleInput = (e: InputEvent) => {
-    const name = (e.target as HTMLInputElement).name;
-    const value = (e.target as HTMLInputElement).value;
-    if (!name || value === undefined || !isKeyofEntryTypeData(name)) {
-      return;
-    }
-
-    setInputs(
-      name as keyof EntryTypeData,
-      { value, error: false, message: '' } as Validation<typeof value>,
-    );
-  };
-
   createEffect(() => {
     if (submitForm.state === 'ready') {
       const result = submitForm() as EntryTypeData;
@@ -76,6 +56,10 @@ export default function EntryTypeEdit(): JSX.Element {
       }
       const { code } = result;
       toasting(`entry type "${code}" has been edited`);
+
+      const names = ['code', 'description', 'unit'];
+      const edited = makeDefaults(result, ...names);
+      setInputs(edited);
     }
   });
 
@@ -119,7 +103,6 @@ export default function EntryTypeEdit(): JSX.Element {
           autoComplete="off"
           sx={{ maxWidth: '10rem' }}
           defaultValue={code}
-          onInput={handleInput}
           error={inputs.code.error}
           helperText={inputs.code.message}
           disabled={isDisabled()}
@@ -133,7 +116,6 @@ export default function EntryTypeEdit(): JSX.Element {
           autoComplete="off"
           sx={{ flex: 1 }}
           defaultValue={description}
-          onInput={handleInput}
           error={inputs.description.error}
           helperText={inputs.description.message}
           disabled={isDisabled()}
