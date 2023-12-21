07a7c95a5f2982a606b95b662119b2e48f953153
innermond <innermond@yahoo.com>
2023-12-21 10:17:53 +0200

    syncing search with columns

--- www/src/components/filter/search.tsx
+++ www/src/components/filter/search.tsx
@@ -5,11 +5,52 @@ import ListSubheader from '@suid/material/ListSubheader';
 import Divider from '@suid/material/Divider';
 import CloseIcon from '@suid/icons-material/Close';
 import IconButton from '@suid/material/IconButton';
-import { For } from 'solid-js';
+import { For, createSignal, createMemo, untrack } from 'solid-js';
 import { produce } from 'solid-js/store';
 import type { FilterProps, FilterState } from './types';
 
+const [search, setSearch] = createSignal();
+
 const FilterSearch = (props: FilterProps) => {
+  if (search() === undefined) {
+    setSearch(props.state.search);
+  }
+
+  // re-build its current state
+  const initialColumns = props.state.initials;
+  // visible after a search has been applied
+  let visibles = [...props.state.items];
+  const initialHidden = initialColumns.filter(
+    (x: string) => !visibles.includes(x),
+  );
+  // show all columns when no search has been applied
+  if (search() === '') {
+    visibles = initialColumns;
+  } else {
+    visibles = initialColumns.filter((x: string) =>
+      x.includes(props.state.search),
+    );
+  }
+
+  const [partColumns, setPartColumns] = createSignal(visibles);
+  const [hiddenOrigin] = createSignal(initialHidden);
+  const hidden = createMemo(() => hiddenOrigin());
+
+  const handleFilterChange = (evt: Event | null, value: string) => {
+    setSearch(value);
+    if (value === '') {
+      const visible = initialColumns.filter(
+        (x: string) => !hidden().includes(x),
+      );
+      setPartColumns(initialColumns);
+      return;
+    }
+    untrack(() => {
+      const found = initialColumns.filter((x: string) => x.includes(value));
+      setPartColumns(found);
+    });
+  };
+
   const handleClose = () => {
     props.setState(
       produce((s: FilterState) => {
@@ -51,13 +92,19 @@ const FilterSearch = (props: FilterProps) => {
         }
       >
         <Divider />
-        <Stack
-          direction="row"
-          divider={<Divider orientation="vertical" flexItem />}
-          sx={{ justifyContent: 'space-evenly' }}
-        ></Stack>
+        <ListItem divider={false} dense>
+          <TextField
+            id="filteringColumns"
+            label="Search by name"
+            variant="filled"
+            size="small"
+            onChange={handleFilterChange}
+            value={search()}
+            autoComplete="off"
+          />
+        </ListItem>
         <Stack direction="column" sx={{ maxHeight: 360, overflow: 'auto' }}>
-          <For each={props.state.items}>
+          <For each={partColumns()}>
             {(item: string) => {
               const id = `search-${item}`;
               return (
